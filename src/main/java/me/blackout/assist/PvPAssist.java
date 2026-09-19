a21a8e96b3c3ba009ae92bd8587cc3c1a69be57d
Blackout180 <Semperx123@gmail.com>
2026-09-19 21:48:54 +0545

    Add mc instance to be initialzed the time when client is called

--- src/main/java/me/blackout/assist/PvPAssist.java
+++ src/main/java/me/blackout/assist/PvPAssist.java
@@ -2,6 +2,7 @@ package me.blackout.assist;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.resources.Identifier;
 
 import org.slf4j.Logger;
@@ -11,10 +12,13 @@ public class PvPAssist implements ModInitializer {
 	public static final String MOD_ID = "pvp-assist";
 	public static final Logger LOG = LoggerFactory.getLogger(MOD_ID);
 
+	public static Minecraft mc;
+
 	@Override
 	public void onInitialize() {
+		LOG.info("Initializing PvP Assist");
 
-		LOG.info("Hello Fabric world!");
+		mc = Minecraft.getInstance();
 	}
 
 	public static Identifier id(String path) {
