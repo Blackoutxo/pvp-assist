bcbd9be758c73c91d70444077bf32aa7dcfd85dd
Blackout180 <Semperx123@gmail.com>
2026-09-21 22:52:26 +0545

    A GUI with test draw

---
+++ src/main/java/me/blackout/assist/gui/GUI.java
@@ -0,0 +1,20 @@
+package me.blackout.assist.gui;
+
+import me.blackout.assist.utils.render.Renderer2D;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+
+public class GUI extends Screen {
+    public GUI() {
+        super(Component.literal("PvPAssist"));
+    }
+
+    @Override
+    public void extractRenderState(@NotNull GuiGraphicsExtractor graphics, int mouseX, int mouseY, float delta) {
+        Renderer2D.drawVerticalLine(graphics, 100, 100, Color.CYAN);
+    }
+}
